6c2dd09ee347d24c0a2df3d7b1bf2829d4979cbe
itsvivekghosh <soapmactevis1@gmail.com>
2020-12-27 18:04:07 +0530

    added file cjhanges

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# CRD_Operations_python
+# CRD Operations using Python
+
+Building a file-based key-value data store for basic CRD Operations ( Creating, Reading and Deleting Key Values).
 
 ### The data store will support the following :
 
@@ -14,3 +16,25 @@
 
 1. The file size never exceeds 1GB
 2. The file is accessed by multi-threading
+
+---
+
+## Working
+
+The main file are main.py and operation.py (inside operations directory) file. inorder to get through the working we can use the commands as:
+
+We have to import the main file to access the operations. And, this can be imported by below line:<br>
+`import operation.operation as op`
+
+1. Creating a Key:
+   `op.create(key_value, value, timeout)`
+
+2. Reading a Key:
+   `op.read(key_value)`
+
+3. Deleting a Key:
+   `op.delete(key_value)`
+
+---
+
+The substequent errors will be generated for the operations.
--- operation/operation.py
+++ operation/operation.py
@@ -6,22 +6,25 @@ import time
 keys = dict()  # 'keys' is the dictionary in which we store data
 
 
-try:
-    def create(key, value, timeout=0):
-        '''
-        For create operation
-        use function create(key_name, value, timeout_value)
-        Here, the timeout value is optional to use
-        '''
+def create(key, value, timeout=0):
+    '''
+    For create operation
+    use function create(key_name, value, timeout_value)
+    Here, the timeout value is optional to use
+    '''
+    try:
         if key in keys:
-            print("Error: The Key already exists")  # error message1
+            print("Error: The Key already exists")  # error message
 
         else:
 
             if(key.isalpha()):
 
                 # constraints for file size less than 1.0GB and JSON object value less than 16KB
-                if len(keys) < (1024*1020*1024) and value <= (16*1024*1024):
+                if (
+                    (len(keys) < (1024 * 1020 *
+                                  1024)) and (value <= (16 * 1024 * 1024))
+                ):
 
                     if timeout == 0:
                         c = [value, timeout]
@@ -29,62 +32,87 @@ try:
                     else:
                         c = [value, timeout + time.time()]
 
-                    if len(key) <= 32:  # constraints for input key_name capped at 32chars
+                    if (len(key) <= 32):  # constraints for input key_name capped at 32chars
                         keys[key] = c
 
                 else:
-                    print("Error: Memory Limit Exceeded! ")  # error message2
+                    print("Error: Memory Limit Exceeded! ")  # error message
 
             else:
-                # error message3
+                # error message
                 print("Error: Invalind key_name!! Key must contain only alphabets!")
-except Exception as e:
-    print(e)
+
+    except Exception as e:
+        print(e)
 
 
 def read(key):
-    ''' 
+    '''
     For read operation of the key
     use function "read(key_name)" to read any key
     '''
-    if key not in keys:
-        # error message4
-        print("Error: Given Key does'nt exists. Please enter a valid key")
-    else:
-        data = keys[key]
-        if data[1] != 0:
-            if time.time() < data[1]:  # comparing the present time with expiry time
-                # to return the value in the format of JSON Object i.e.,"key_name: value"
-                stri = str(key)+":"+str(data[0])
-                return stri
-            else:
-                print("Error: Time-To-Live of '", key,
-                      "' has been Expired")  # error message5
+    try:
+
+        if key not in keys:
+            # error message
+            print("Error: Given Key does'nt exists. Please enter a valid key")
+
         else:
-            stri = str(key)+":"+str(data[0])
-            return stri
+
+            data = keys[key]
+
+            if data[1] == 0:
+                data = str(key) + ":" + str(data[0])
+                return data
+
+            else:
+                # comparing the present time w.r.t expiry time
+                if time.time() < data[1]:
+
+                    # to return the value in the format of JSON Object in the form of "key_name: value"
+                    data = str(key) + ":" + str(data[0])
+                    return data
+
+                else:
+                    print("Error: Time-To-Live of '", key,
+                          "' has been Expired")  # error message
+
+    except Exception as e:
+        print(e)
 
 
 def delete(key):
-    ''' 
+    '''
     For delete operation of the key value
     use function "delete(key_name)" to delete any key
     '''
-    if key not in keys:
-        # error message4
-        print("Error: Key does not exists. Please enter a valid key")
-    else:
-        oper = keys[key]
-        if oper[1] != 0:
-            if time.time() < oper[1]:  # comparing the current time with expiry time
-                del keys[key]
-                print("Key Deleted Successfully!!")
-            else:
-                print("Error: Time-To-Live of '", key,
-                      "' has been expired")  # error message5
+    try:
+
+        if key not in keys:
+            # error message
+            print("Error: Key does not exists. Please enter a valid key")
+
         else:
-            del keys[key]
-            print("Key is successfully deleted!!")
+
+            oper = keys[key]
+
+            if oper[1] != 0:
+
+                # comparing the current time w.r.t expiry time
+                if time.time() < oper[1]:
+                    del keys[key]
+                    print("Key Deleted Successfully!!")
+
+                else:
+                    print("Error: Time-To-Live of '", key,
+                          "' has been expired")  # error message
+
+            else:
+                del keys[key]
+                print("Key is successfully deleted!!")
+
+    except Exception as e:
+        print(e)
 
 
 def modify(key, value):
@@ -92,27 +120,39 @@ def modify(key, value):
     For modify operation (modifying the key)
     use function "modify(key_name, new_value)"
     '''
-    oper = keys[key]
-    if oper[1] != 0:
-        if time.time() < oper[1]:
+    try:
+
+        oper = keys[key]  # taking out the key from keys
+
+        if oper[1] == 0:
+
             if key not in keys:
-                # error message6
-                print(
-                    "Error: Key does'nt exists. Please enter a valid key")
+                # error message
+                print("Error: Key does'nt exists. Please enter a valid key")
+
             else:
                 values = []
                 values.append(value)
                 values.append(oper[1])
                 keys[key] = values
+
         else:
-            print("Error: Time-To-Live of '", key,
-                  "' has been expired")  # error message5
-    else:
-        if key not in keys:
-            # error message6
-            print("Error: Key does'nt exists. Please enter a valid key")
-        else:
-            values = []
-            values.append(value)
-            values.append(oper[1])
-            keys[key] = values
+            if time.time() < oper[1]:
+
+                if key not in keys:
+                    # error message
+                    print(
+                        "Error: Key does'nt exists. Please enter a valid key")
+
+                else:
+                    values = []
+                    values.append(value)
+                    values.append(oper[1])
+                    keys[key] = values
+
+            else:
+                print("Error: Time-To-Live of '", key,
+                      "' has been expired")  # error message
+
+    except Exception as e:
+        print(e)
